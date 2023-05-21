c26defafa04b4b2078669842d77774d9acd39f79
Denis <hallo-boy@mail.ru>
2023-05-21 09:26:49 +0300

    days data

--- js/script.js
+++ js/script.js
@@ -17,10 +17,10 @@ function showPosition(position) {
       let myData = data.features[0].properties.city;
       getCityData(myData);
       getWeather(myData);
+      getDayData(myData);
     });
 }
 getCityData = (data) => {
-  console.log(data);
   let city = document.querySelector(".main-data__city");
   city.textContent = data;
 };
@@ -54,9 +54,68 @@ function weatherData(data) {
   document.getElementById(
     "air-humidity"
   ).textContent = `${nowWeather.main.humidity} %`;
-  const firstKey = Object.keys(nowWeather.rain)[0];
-const firstValue = nowWeather.rain[firstKey];
-  document.getElementById("rainfall").textContent = `${firstValue} мм`;
+  let rainfall = document.getElementById("rainfall");
 
-  console.log(data);
+  if (nowWeather.rain) {
+    const firstKey = Object.keys(nowWeather.rain)[0];
+    if (typeof firstKey !== "undefined") {
+      const firstValue = nowWeather.rain[firstKey];
+      rainfall.textContent = `${firstValue} мм`;
+    } else {
+      rainfall.textContent = "Без осадков";
+    }
+  } else {
+    rainfall.textContent = "Без осадков";
+  }
 }
+
+function getDayData(city) {
+  const apiKey = "076bcf111ff04827b46625e186673128";
+  const apiUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&days=6&lang=ru&key=${apiKey}`;
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      let days = data;
+      dayData(days);
+    })
+    .catch((error) => {
+      // Обработка ошибок
+      console.log(error);
+    });
+}
+
+function dayData(days) {
+  const daysOfWeek = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+  ];
+  let oneDayTitle = document.querySelectorAll(".week-data__title");
+  let oneDayTemperature = document.querySelectorAll(
+    ".week-data__temperature-day"
+  );
+  console.log(days.data);
+  let daysData = days.data;
+  for (let i = 0; i < daysData.length - 1; i++) {
+    const date = new Date(daysData[i + 1].datetime);
+    const dayOfWeek = daysOfWeek[date.getDay()];
+    console.log(dayOfWeek);
+    console.log(daysData[i]);
+    console.log(oneDayTitle[i]);
+    oneDayTemperature[i].textContent = `${Math.round(
+      daysData[i + 1].app_max_temp
+    )} °`;
+    oneDayTitle[i].textContent = dayOfWeek;
+  }
+}
+
+// const dateStr = "2023-05-22";
+// const date = new Date(dateStr);
+// const daysOfWeek = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
+// const dayOfWeek = daysOfWeek[date.getDay()];
+
+// console.log(`День недели: ${dayOfWeek}`);
